f6fcd18ea1b0a1a1c40a7a95681b484264da13ee
jtmbeta <joel.t.martin36@gmail.com>
2021-03-19 15:24:31 +0000

    rebuilt docs /JTM

--- matlab/legacy/bv_setSettings.m
+++
@@ -1,6 +0,0 @@
-function bv_setSettings(dev, primaries)
-
-theSpecString = sprintf('%d,', primaries);
-theSpecString(end) = '';
-
-webread([dev.url_module,'/api/luminaire/', num2str(dev.device_id), '/spectruma/' theSpecString], dev.opts);
--- matlab/legacy/bv_turnAllOff.m
+++
@@ -1,3 +0,0 @@
-function bv_turnAllOff
-dev = bv_setupDevice();
-bv_setSettings(dev, [0 0 0 0 0 0 0 0 0 0]);
